842d3641fb8c4af8f88c0e88559bb6d7f257225b
kevindoveton <kevindoveton@me.com>
2016-11-13 17:12:05 +1030

    updated auth to use sqlite database

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var storage = require('./lib/memstorage.js');
 // var storage = require('./lib/perstorage.js');
-var authModule = require('./lib/auth.js')
+var authModule = require('./lib/auth.js')({
+	path: './email.db'
+});
 // you now can pass storage to imapper
 server = require('./lib/server.js')({
     storage: storage,
